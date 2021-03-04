caf3e27a5943f029f8ae8e2c746d115ee6e3eb60
Randy Reiza <randy.aries.95@gmail.com>
2021-03-04 11:40:12 +0700

    MyTestingApp (P2) - Error IndexOutBoundsException pada array yg dipanggil lbh dr indexnya

--- MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
+++ MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +21,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         tvText = findViewById(R.id.tv_text)
         btnSetValue = findViewById(R.id.btn_set_value)
         btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
     }
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_set_value) {
-            tvText.text = "19"
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
