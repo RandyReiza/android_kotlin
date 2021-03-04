3fe51b7695d96b8b100d18890e64a24d258a5b41
Randy Reiza <randy.aries.95@gmail.com>
2021-03-04 11:12:26 +0700

    MyTestingApp (P1) - Error NullPointerException pada button yang belum diinisiasi

--- MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
+++ MyTestingApp/app/src/main/java/com/example/mytestingapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_set_value) {
+            tvText.text = "19"
+        }
     }
 }
