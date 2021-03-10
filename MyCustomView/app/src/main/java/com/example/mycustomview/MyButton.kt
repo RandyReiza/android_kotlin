c1c776b0c3541d9561dd089bb3f9c99895d4a9dd
Randy Reiza <randy.aries.95@gmail.com>
2021-03-10 12:02:08 +0700

    MyCustomView (P1) - Membuat custom view dari Edit Text dan Button, serta behaviournya

---
+++ MyCustomView/app/src/main/java/com/example/mycustomview/MyButton.kt
@@ -0,0 +1,43 @@
+package com.example.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+
+class MyButton : AppCompatButton {
+
+    private var enableBackground: Drawable? = null
+    private var disableBackground: Drawable? = null
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    // utk mengubah bentuk view
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        background = if (isEnabled) enableBackground else disableBackground
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = if (isEnabled) "Submit" else "Isi Dulu"
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enableBackground = ResourcesCompat.getDrawable(resources, R.drawable.bg_button, null)
+        disableBackground = ResourcesCompat.getDrawable(resources, R.drawable.bg_button_disable, null)
+    }
+}
