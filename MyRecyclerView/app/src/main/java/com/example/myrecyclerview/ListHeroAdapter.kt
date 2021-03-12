f9031d5dd1a2966cc994e8f71088a2773d7b0070
Randy Reiza <randy.aries.95@gmail.com>
2021-03-12 08:08:10 +0700

    MyRecyclerView (P2) - Membuat data adapter ListHeroAdapter serta data class Hero untuk Parcelable

---
+++ MyRecyclerView/app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.myrecyclerview.databinding.ItemRowHeroBinding
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    inner class ListViewHolder(private val binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(hero: Hero) {
+            with(binding) {
+                Glide.with(itemView.context)
+                        .load(hero.photo)
+                        .apply(RequestOptions().override(55, 55))
+                        .into(imgItemPhoto)
+
+                tvItemName.text = hero.name
+                tvItemDescription.text = hero.description
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, i: Int) {
+        holder.bind(listHero[i])
+    }
+
+    override fun getItemCount(): Int = listHero.size
+}
