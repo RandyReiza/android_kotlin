50d16f1b9c7677923a68c332e7c1fc638a66fae8
Randy Reiza <randy.aries.95@gmail.com>
2021-03-12 08:15:36 +0700

    MyRecyclerView (P4) - Membuat & menampilkan 3 dot menu utk menu2 dr mode view yang akan dibuatkan nantinya

--- MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -46,4 +46,27 @@ class MainActivity : AppCompatActivity() {
         binding.rvHeroes.adapter = listHeroAdapter
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+
+            }
+            R.id.action_cardview -> {
+
+            }
+        }
+    }
 }
